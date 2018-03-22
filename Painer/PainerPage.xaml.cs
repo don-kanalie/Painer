faf4704c1b39d63b08dc9b07ae44d69df19b306d
Marcel Teppner <m.teppner@secova.de>
2018-03-22 14:25:48 +0100

    initial project structure

---
+++ Painer/PainerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Painer
+{
+    public partial class PainerPage : ContentPage
+    {
+        public PainerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
